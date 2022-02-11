1513bade8aa05a68d6db40838bed15164771f75f
Nathan Yee <nyee@csu.fullerton.edu>
2022-02-11 09:46:16 -0800

    user card colors implemented

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,10 +1,17 @@
 export default function UserCard(props) {
 
     return(
-        <div className="bg-card h-full rounded-xl">
-            <img className="h-[100px] w-[100px] rounded-full border-4 border-white" src={props.imgSrc} alt="profile" />
-            <p className="text-sm text-slate-300 font-light">Report For</p>
-            <h1 className="text-[18px] text-white">{props.name}</h1>
+        <div className="md:relative bg-card h-full rounded-xl">
+            <div className="md:absolute bg-profile h-3/4 w-full rounded-xl">
+                <div className="pl-7 pt-7">
+                    <img className="h-[100px] w-[100px] rounded-full border-4 border-white" src={props.imgSrc} alt="profile" />
+                </div>
+                <p className="pl-10 pt-10 text-lg text-slate-300 font-light">Report For</p>
+                <h1 className="pl-10 text-6xl pt-3 text-white">{props.name}</h1>
+            </div>
+            <p className="absolute text-substat bottom-0">Daily</p>
+            <p className="absolute text-substat bottom-10">Daily</p>
+            <p className="absolute text-substat bottom-5">Daily</p>
         </div>
     );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         <>
             <div className="md:grid grid-rows-2 grid-flow-col gap-6 mx-auto py-10 h-[38rem] max-w-7xl">
                 <div className="md:row-span-2">
-                    <UserCard name="Nathan Yee" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCdyi-jV5u0bVQC1BIH0LAH2wJT_p56qUNQ&usqp=CAU" />
+                    <UserCard name="John Doe" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCdyi-jV5u0bVQC1BIH0LAH2wJT_p56qUNQ&usqp=CAU" />
                 </div>
                 <Work hours="10" lastWeek="9"/>
                 <Exercise hours="10" lastWeek="9"/>
